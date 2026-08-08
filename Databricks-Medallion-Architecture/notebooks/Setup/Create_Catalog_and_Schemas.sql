c73bb1e11cb7f695a4973e815075c84b1d351fb2
Sandeep Tanniru <sandeeptanniru98@gmail.com>
2026-08-08 19:16:22 +0530

    Update Create_Catalog_and_Schemas.sql

--- Databricks-Medallion-Architecture/notebooks/Setup/Create_Catalog_and_Schemas.sql
+++ Databricks-Medallion-Architecture/notebooks/Setup/Create_Catalog_and_Schemas.sql
@@ -1,1 +1,11 @@
+--- catalog creation
 
+CREATE CATALOG retail_catalog;
+
+--- schema creation
+
+CREATE SCHEMA retail_catalog.raw;
+
+CREATE SCHEMA retail_catalog.curated;
+
+CREATE SCHEMA retail_catalog.analytics;
